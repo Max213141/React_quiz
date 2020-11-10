af5a5dad175f98f873e4b64b3ecd1600950c228e
Max213141 <66839835+Max213141@users.noreply.github.com>
2020-11-10 18:11:15 +0300

    just for commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import reportWebVitals from './reportWebVitals';
+import rootReducer from "./store/reducers/rootReducer";
+
+const store = createStore(rootReducer())
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store = {store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render( app,  document.getElementById('root'));
